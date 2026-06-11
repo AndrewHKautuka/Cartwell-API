310b00ba9fab827ff556932cf5f59555df69f425
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2026-06-11 11:55:49 +0200

    Initial commit

---
+++ Cartwell/Controllers/WeatherForecastController.cs
@@ -0,0 +1,28 @@
+using Cartwell.Weather.Types;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cartwell.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+	private static readonly string[] Summaries =
+	[
+		"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot",
+		"Sweltering", "Scorching"
+	];
+
+	[HttpGet(Name = "GetWeatherForecast")]
+	public IEnumerable<WeatherForecast> Get()
+	{
+		return Enumerable.Range(1, 5)
+						 .Select(index => new WeatherForecast
+										  {
+											  Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+											  TemperatureC = Random.Shared.Next(-20, 55),
+											  Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+										  })
+						 .ToArray();
+	}
+}

704c033699a0c3022576203ef1f92dff77c9fafc
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2026-06-12 10:45:17 +0200

    Simplify LocalDate schema and add NodaTime serialization support
    - Simplify LocalDate OpenAPI schema to standard ISO 8601 date format
    - Remove verbose NodaTime type definitions (CalendarSystem, Era, IsoDayOfWeek)
    - Add MMonrad.OpenApi.NodaTime package for OpenAPI schema generation
    - Add NodaTime.Serialization.SystemTextJson for JSON serialization
    - Configure NodaTime serialization in JSON options with Tzdb provider
    - Configure NodaTime in OpenAPI document generation
    - Reduces OpenAPI schema complexity while maintaining proper date handling

--- Cartwell/Program.cs
+++ Cartwell/Program.cs
@@ -2,6 +2,8 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Http.Json;
 using NodaTime;
+using NodaTime.Serialization.SystemTextJson;
+using OpenApi.NodaTime.Extensions;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,9 +12,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Singletons
 builder.Services.AddSingleton<IClock>(SystemClock.Instance);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+	   .AddJsonOptions(options =>
+	   {
+		   options.JsonSerializerOptions.ConfigureForNodaTime(DateTimeZoneProviders.Tzdb);
+	   });
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi("v1");
+builder.Services.AddOpenApi("v1",
+							options =>
+							{
+								options.ConfigureNodaTime();
+							});
 
 builder.Services.Configure<JsonOptions>(options =>
 {

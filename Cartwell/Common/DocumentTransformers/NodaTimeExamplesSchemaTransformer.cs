56e5f051de275859059af601c6ed3ce9253f13cd
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2026-06-21 13:06:37 +0200

    Expand schema examples for additional NodaTime types
    - Add LocalTime example (13:45:30)
    - Add LocalDateTime example (2026-06-12T13:45:30)
    - Add UTC-based examples for Instant, ZonedDateTime, and OffsetDateTime
    - Add OffsetTime example with timezone offset (13:45:30+00:00)
    - Add OffsetDate example with timezone offset (2026-06-12+00:00)
    - Add Period example using ISO 8601 duration format (P1Y2M10DT2H30M)
    - Add DateInterval example with Start and End dates
    - Add Interval example with Start and End instants
    - Completes OpenAPI schema documentation for the full NodaTime type hierarchy

--- Cartwell/Common/DocumentTransformers/NodaTimeExamplesSchemaTransformer.cs
+++ Cartwell/Common/DocumentTransformers/NodaTimeExamplesSchemaTransformer.cs
@@ -14,6 +14,30 @@ public sealed class NodaTimeExamplesSchemaTransformer : IOpenApiSchemaTransforme
 
 		if (type == typeof(LocalDate))
 			schema.Example = "2026-06-12";
+		else if (type == typeof(LocalTime))
+			schema.Example = "13:45:30";
+		else if (type == typeof(LocalDateTime))
+			schema.Example = "2026-06-12T13:45:30";
+		else if (type == typeof(Instant)
+				 || type == typeof(ZonedDateTime)
+				 || type == typeof(OffsetDateTime))
+			schema.Example = "2026-06-12T13:45:30Z";
+		else if (type == typeof(OffsetTime))
+			schema.Example = "13:45:30+00:00";
+		else if (type == typeof(OffsetDate))
+			schema.Example = "2026-06-12+00:00";
+		else if (type == typeof(Period))
+			schema.Example = "P1Y2M10DT2H30M";
+		else if (type == typeof(DateInterval) && schema.Properties is { } dp)
+		{
+			((OpenApiSchema)dp["Start"]).Example = "2026-06-12";
+			((OpenApiSchema)dp["End"]).Example = "2026-06-20";
+		}
+		else if (type == typeof(Interval) && schema.Properties is { } ip)
+		{
+			((OpenApiSchema)ip["Start"]).Example = "2026-06-12T13:45:30Z";
+			((OpenApiSchema)ip["End"]).Example = "2026-06-20T16:16:31Z";
+		}
 
 		return Task.CompletedTask;
 	}
